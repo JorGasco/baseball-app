fb40487f279f5f5e2d64d3cf4909b6c8564a9b49
Jorge <20099103@mail.wit.ie>
2023-04-30 23:55:00 +0100

    Creat models,controllers to store the list for the players

--- src/main/kotlin/controllers/PlayersAPI.java
+++ src/main/kotlin/controllers/PlayersAPI.java
@@ -1,4 +1,12 @@
-package controllers;
+package controllers
 
-public class PlayersAPI {
+import models.Players
+import persistence.Serializer
+
+class PlayersAPI(serializerType: Serializer) {
+
+    private var serializer: Serializer = serializerType
+
+    private var players = ArrayList<Players>()
+
 }
---
+++ src/main/kotlin/models/Players.kt
@@ -0,0 +1,12 @@
+package models
+
+data class Players(
+
+var playerName: String,
+var playerSurname: String,
+var age: Int,
+var height: Double,
+var weight: Double,
+var position: String,
+var isNoteArchived: Boolean)
+{}
